ab1d598d0dab9ad2292d0f3307faa4c59e796ac4
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-07 18:15:28 +0900

    RSpec導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
+    user_birth_date       {Faker::Date.between(from: '1930-01-01', to: '2017-12-31')}
+    transient do
+      person { Gimei.name }
+   end
+   first_name { person.first.kanji }
+   last_name { person.last.kanji }
+   first_name_kana { person.first.katakana }
+   last_name_kana { person.last.katakana }
   end
 end
