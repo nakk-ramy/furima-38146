0a498014f9ec4ebce9d34c72772bd2e12999e173
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-07 16:12:44 +0900

    バリデーション、カラム情報をDB保存

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :user_birth_date, presence: true
+  validates :password, {format: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+end
