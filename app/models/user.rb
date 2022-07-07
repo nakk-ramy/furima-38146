f902118e7943e940742d71d586b9345ba7098724
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-07 18:53:24 +0900

    単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :user_birth_date, presence: true
-  validates :password, {format: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
-  validates :last_name, {format: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name, {format: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :last_name_kana, {format: /\A[ァ-ヶー]+\z/ }
-  validates :first_name_kana, {format: /\A[ァ-ヶー]+\z/ }
+  validates :password, { format: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+  validates :last_name, { format: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, { format: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, { format: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, { format: /\A[ァ-ヶー]+\z/ }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
-    password_confirmation {password}
-    user_birth_date       {Faker::Date.between(from: '1930-01-01', to: '2017-12-31')}
+    nickname              { Faker::Name.initials }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password_confirmation { password }
+    user_birth_date       { Faker::Date.between(from: '1930-01-01', to: '2017-12-31') }
     transient do
       person { Gimei.name }
-   end
-   first_name { person.first.kanji }
-   last_name { person.last.kanji }
-   first_name_kana { person.first.katakana }
-   last_name_kana { person.last.katakana }
+    end
+    first_name { person.first.kanji }
+    last_name { person.last.kanji }
+    first_name_kana { person.first.katakana }
+    last_name_kana { person.last.katakana }
   end
 end
