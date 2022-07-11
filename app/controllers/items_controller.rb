2d0cb68af957e6ecd975402aaa52cb71b4ef79ff
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-11 16:50:10 +0900

    商品出品機能(JS以外)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,20 @@
-class ItemsController < ApplicationController
+class ItemsController < ApplicationController 
   def new
     @item = Item.new
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image, :category_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :price, :content, :image, :category_id, :sales_status_id, :shipping_free_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
   end
 end
