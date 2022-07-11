8a391aff4c04ab2910e1df25fb23fd49637d8615
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-11 18:54:58 +0900

    バリデーション(金額)

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,9 @@ class Item < ApplicationRecord
   validates :image,                   presence: true
   validates :name,                    presence: true
   validates :text,                    presence: true
-  validates :price,                   presence: true
+  VALID_PRICEL_HALF = /\A[0-9]+\z/
+  validates :price, presence: true, numericality: { with: VALID_PRICEL_HALF, message: 'Half-width number' }
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   validates :category_id,             presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_free_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
