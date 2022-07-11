f6375bf867a04d0f07d2bb62023aa86953c5fa72
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-11 18:17:00 +0900

    販売手数料、販売利益JS

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,13 +5,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image,                   presence: true
   validates :name,                    presence: true
   validates :text,                    presence: true
+  validates :price,                   presence: true
   validates :category_id,             presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_free_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :scheduled_delivery_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,                   presence: true
-  validates :image,                   presence: true
 end
