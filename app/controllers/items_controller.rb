ed0ba5916e5aced12ae69044458d711b1b3d63b5
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-11 12:04:04 +0900

    「カテゴリー」アクティブハッシュ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:content, :image, :category_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
   has_one_attached :image
 
   validates :name,                    presence: true
   validates :text,                    presence: true
-  validates :category_id,             presence: true
-  validates :sales_status_id,         presence: true
-  validates :shipping_free_status_id, presence: true
-  validates :prefecture_id,           presence: true
-  validates :scheduled_delivery_id,   presence: true
+  validates :category_id,             presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_free_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,                   presence: true
   validates :image,                   presence: true
 end
