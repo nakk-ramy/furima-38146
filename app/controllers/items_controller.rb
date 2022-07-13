bc04b349a4f3e3c2e33d8f70fb5e90d95542cffa
nakk-ramy <nakki.7015.alan@gmail.com>
2022-07-13 17:15:48 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,10 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-    end
+    redirect_to root_path if @item.destroy
   end
 
   private
